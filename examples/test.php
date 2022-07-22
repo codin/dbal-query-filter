d88cb6ecda9a9e84f0087537bfb3116ffc0124ef
Kieron <kieron@fastmail.fm>
2022-07-22 11:45:11 +0100

    wip

--- examples/test.php
+++ examples/test.php
@@ -8,14 +8,15 @@ require __DIR__ . '/../vendor/autoload.php';
 $conn = Doctrine\DBAL\DriverManager::getConnection(['driver' => 'pdo_sqlite', 'path' => ':memory:']);
 $query = $conn->createQueryBuilder()->from('tbl')->select('*');
 
-parse_str('filters[product]=1&filters[category][]=games&filters[amount_gte]=2000&fields[]=id', $params);
+parse_str('filters[product]=1&filters[category][]=games&filters[amount_gte]=2000&fields[]=id&filters[show_deleted]=1', $params);
 
 $filter = (new Codin\DBAL\QueryFilter())
     ->match('product')
     ->range('amount')
     ->contains('category')
+    ->nullable('show_deleted', 'deleted_at', true)
 ;
 
 $filter->build($query, $params['filters']);
 
-var_dump($query->getSQL(), $query->getParameters());
+var_dump($params, $query->getSQL(), $query->getParameters());
--- src/QueryFilter.php
+++ src/QueryFilter.php
@@ -5,7 +5,15 @@ declare(strict_types=1);
 namespace Codin\DBAL;
 
 use Doctrine\DBAL\Query\QueryBuilder;
+use ReflectionClass;
 
+/**
+ * @method self callback(string $queryName, callable $callback)
+ * @method self contains(string $queryName, ?string $columnName, bool $notIn)
+ * @method self match(string $queryName, ?string $columnName)
+ * @method self nullable(string $queryName, ?string $columnName, bool $notNull)
+ * @method self range(string $queryName, ?string $columnName)
+ */
 class QueryFilter
 {
     protected array $definitions = [];
@@ -20,21 +28,31 @@ class QueryFilter
     public function __call(string $method, array $args): self
     {
         $name = __NAMESPACE__ . '\\Filters\\'.ucfirst($method);
-        if (class_exists($name)) {
+        if (class_exists($name) && (new ReflectionClass($name))->isInstantiable()) {
             return $this->map(new $name(...$args));
         }
         throw new \ErrorException('Filter does not exist: '.$name);
     }
 
-    public function build(QueryBuilder $query, array $params): void
+    /**
+     * Matched filters from input array
+     */
+    protected function getFilters(array $params): array
     {
-        $filters = array_filter(
+        return array_filter(
             $this->definitions,
             static function (Filters\Filter $filter) use ($params): bool {
                 return $filter->validate($params);
             }
         );
-        foreach ($filters as $filter) {
+    }
+
+    /**
+     * Mutable query builder
+     */
+    public function build(QueryBuilder $query, array $params): void
+    {
+        foreach ($this->getFilters($params) as $filter) {
             $filter->build($query, $params);
         }
     }
